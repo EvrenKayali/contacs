881865b301438ad7fe195e5b9833785f444c57f6
Evren Kayali <evren.kayali@readify.net>
2019-03-16 19:27:58 +1100

    feat: add contact list

---
+++ src/Components/ContactList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Contact } from "../Models/Contact";
+
+export interface Props {
+  contacts: Contact[];
+}
+
+export function ContactList(props: Props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Phone Number</th>
+        </tr>
+      </thead>
+      {props.contacts.map(contact => (
+        <tr>
+          <td>{contact.name}</td>
+          <td>{contact.phoneNumber}</td>
+        </tr>
+      ))}
+    </table>
+  );
+}
---
+++ src/Components/Contacts.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Contact } from "../Models/Contact";
+import { ContactList } from "./ContactList";
+
+export interface State {
+  contacts: Contact[];
+}
+
+export class Contacts extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      contacts: [
+        { name: "Evren", phoneNumber: "5326107664" },
+        { name: "Kaya", phoneNumber: "4146028" }
+      ]
+    };
+  }
+
+  render() {
+    return <ContactList contacts={this.state.contacts} />;
+  }
+}
