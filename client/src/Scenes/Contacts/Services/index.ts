88aada61e011e9a5c3791a64769260cfebb546f2
Evren Kayali <evren.kayali@readify.net>
2019-03-24 02:55:25 +1100

    feat: add back seacrh

--- client/src/Scenes/Contacts/Services/index.ts
+++ client/src/Scenes/Contacts/Services/index.ts
@@ -4,13 +4,15 @@ import { Action } from "redux";
 import { Contact } from "../Models/Contact";
 import { populateContacts } from "../../../Store/Actions";
 
-export const fetchContacts = (): ThunkAction<
-  void,
-  AppState,
-  null,
-  Action
-> => async dispatch => {
-  fetch("https://localhost:5001/api/contacts")
+export const fetchContacts = (
+  searchTerm?: string
+): ThunkAction<void, AppState, null, Action> => async dispatch => {
+  const uri =
+    searchTerm == null
+      ? "https://localhost:5001/api/contacts"
+      : `https://localhost:5001/api/contacts?filter=${searchTerm}`;
+
+  fetch(uri)
     .then(response => response.json())
     .then((contacts: Contact[]) => dispatch(populateContacts(contacts)));
 };
--- client/src/Scenes/Contacts/index.tsx
+++ client/src/Scenes/Contacts/index.tsx
@@ -1,43 +1,49 @@
 import React from "react";
-import { Contact } from "./Models/Contact";
 import { ContactList } from "./Components/ContactList";
 import { ContactsState } from "../../Store/Types";
 import { AppState } from "../../Store";
 import { connect } from "react-redux";
 import { changeSelection } from "../../Store/Actions";
 import { fetchContacts } from "./Services";
-import { throws } from "assert";
+import ContactSearchInput from "./Components/ContactSearchInput";
+import { ThunkAction } from "redux-thunk";
+import { Action } from "redux";
 
 export interface Props {
   contactState: ContactsState;
   changeSelection: typeof changeSelection;
-  fetchContacts: any;
+  fetchContacts: (term?: string) => void;
 }
 
 export class Contacts extends React.Component<Props> {
-  toggleContactSelection = (selectedContact: Contact) => {
-    this.props.changeSelection(selectedContact);
-  };
-
   componentDidMount() {
     this.props.fetchContacts();
   }
+
+  onSearch = (searchTerm: string) => {
+    if (searchTerm.length >= 3 || searchTerm.length == 0) {
+      this.props.fetchContacts(searchTerm);
+    }
+  };
 
   render() {
     return (
       <React.Fragment>
         <div className="row mt-3">
-          <div className="col" />
+          <div className="col">
+            <ContactSearchInput onSearch={term => this.onSearch(term)} />
+          </div>
         </div>
         <div className="row mt-3">
           <div className="col">
             <ContactList
               contacts={this.props.contactState.contacts}
-              selectionChanged={e => this.toggleContactSelection(e)}
+              selectionChanged={selectedContact =>
+                this.props.changeSelection(selectedContact)
+              }
             />
           </div>
         </div>
-        {JSON.stringify(this.props.contactState.contact)}
       </React.Fragment>
     );
   }
